1fc072115932a150fb262e8769b186e72ebeb511
adas <ajanta744@gmail.com>
2025-03-05 11:18:37 +1100

    Updated navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,9 +1,53 @@
-import React from 'react'
+'use client';
 
-const Navbar = () => {
+import { useState } from 'react';
+import Link from 'next/link';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+import styles from './navbar.module.css';
+
+export default function HeroSection() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <section>
+      {/* Navigation */}
+      <nav className={styles.nav}>
+        <div className={styles.logo}>LOGO</div>
+        <ul className={menuOpen ? styles.active : ''}>
+          <li><Link href="#about">About</Link></li>
+          <li><Link href="#service">Service</Link></li>
+          <li><Link href="#contact">Contact</Link></li>
+        </ul>
+        <div className={styles.menuToggle} onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+      </nav>
 
-export default Navbar
+      {/* Hero Content */}
+      <div className={styles.heroContent}>
+        <h1>Heading Here</h1>
+        <button>Get Started</button>
+        <div className={styles.tabs}>
+          <button>iOS Version</button>
+          <button>Android Version</button>
+        </div>
+      </div>
+
+      {/* Hero Image & Social Icons */}
+      <div className={styles.heroImage}>
+        <div className={styles.socialIcons}>
+          <a href="#"><FaFacebookF /></a>
+          <a href="#"><FaInstagram /></a>
+          <a href="#"><FaTwitter /></a>
+        </div>
+      </div>
+
+      {/* Features */}
+      <div className={styles.features}>
+        <div className={styles.feature}> Feature 1</div>
+        <div className={styles.feature}> Feature 2</div>
+        <div className={styles.feature}> Feature 3</div>
+      </div>
+    </section>
+  );
+}
