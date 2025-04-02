1c56ca61846153bcdcb0f3ebb55bbb027364e21f
Ajanta Das <ajanta744@gmail.com>
2025-04-02 14:53:36 +1100

    Updated portfolioData.js

--- components/Portfolio/portfolioData.js
+++ components/Portfolio/portfolioData.js
@@ -1,49 +1,75 @@
 const portfolioData = [
-    {
-      id: 1,
-      image: "/imgs/lence.png",
-      title: "Photography Website",
-      description:
-        "A modern and visually appealing site showcasing high-resolution images with an interactive gallery. Applied HTML, CSS, JavaScript, and modern frameworks like React.",
-        website: "https://melbournelence.com/",
-        github: "https://github.com/das744/melbournelence",
-    },
-    {
-      id: 2,
-      image: "/imgs/OMM.png",
-      title: "Hydraulic Manual Website",
-      description:
-        "A structured site providing easy access to hydraulic manuals, troubleshooting guides, and instructional content.",
-        website: "https://hydraulicomm.com.au/",
-        github:"https://github.com/das744/hydraulicOMM",
-    },
-    {
-      id: 3,
-      image: "/imgs/salon.png",
-      title: "Beauty Parlor Website",
-      description: "A stylish website featuring service listings, an online booking system, and a sleek, user-friendly design.",
-      website: "https://doreenbeautyparlour.com/",
-      github: "https://github.com/das744/beautysalon",
-    },
-    {
-      id: 4,
-      image: "/imgs/tutoring.png",
-      title: "Math Tutoring Website",
-      description:
-        "n educational platform offering tutor booking, and interactive resources.Created using React, HTML, CSS, and JS.",
-        website: "https://fredostutoring.com/",
-        github: "https://github.com/das744/mathtutor",
-    },
-    {
-      id: 5,
-      image: "/imgs/WDM.png",
-      title: "Web Design Website",
-      description: "An educational platform offering tutor booking, and interactive resources.",
-      website: "https://www.webdeveloperinmelbourne.com.au/",
-      github: "https://github.com/wdm",
-    },
-   
-  ];
-  
-  export default portfolioData;
+  {
+    id: 1,
+    image: "/imgs/lence.png",
+    title: "Photography Website",
+    description:
+      "A modern and visually appealing site showcasing high-resolution images with an interactive gallery. Applied HTML, CSS, JavaScript, and modern frameworks like React.",
+      website: "https://melbournelence.com/",
+      github: "https://github.com/das744/melbournelence",
+  },
+  {
+    id: 2,
+    image: "/imgs/OMM.png",
+    title: "Hydraulic Manual Website",
+    description:
+      "A structured site providing easy access to hydraulic manuals, troubleshooting guides, and instructional content.",
+      website: "https://hydraulicomm.com.au/",
+      github:"https://github.com/das744/hydraulicOMM",
+  },
+  {
+    id: 3,
+    image: "/imgs/salon.png",
+    title: "Beauty Parlor Website",
+    description: "A stylish website featuring service listings, an online booking system, and a sleek, user-friendly design.",
+    website: "https://doreenbeautyparlour.com/",
+    github: "https://github.com/das744/beautysalon",
+  },
+  {
+    id: 4,
+    image: "/imgs/tutoring.png",
+    title: "Math Tutoring Website",
+    description:
+      "n educational platform offering tutor booking, and interactive resources.Created using React, HTML, CSS, and JS.",
+      website: "https://fredostutoring.com/",
+      github: "https://github.com/das744/mathtutor",
+  },
+  {
+    id: 5,
+    image: "/imgs/WDM.png",
+    title: "Web Design Website",
+    description: "An educational platform offering tutor booking, and interactive resources.",
+    website: "https://www.webdeveloperinmelbourne.com.au/",
+    github: "https://github.com/wdm",
+  },
+  {
+    id: 6,
+    image: "/imgs/WDM.png",
+    title: "Web Design Website",
+    description: "Photography Website.",
+    website: "http://photography.com.s3-website-ap-southeast-2.amazonaws.com/",
+    github: "https://github.com/das744/new_born_photography",
+  },
+  {
+    id: 7,
+    image: "/imgs/WDM.png",
+    title: "Web Design Website",
+    description: "An educational platform offering tutor booking, and interactive resources.",
+    website: "https://merndaguitarlesson.vercel.app/",
+    github: "https://github.com/das744/nextjs-theme",
+  },
+  {
+    id: 8,
+    image: "/imgs/WDM.png",
+    title: "Web Design Website",
+    description: "An educational platform offering tutor booking, and interactive resources.",
+    website: "https://merndaguitarlesson.vercel.app/",
+    github: "https://github.com/das744/dental_clinic",
+  },
+
+ 
+];
+
+export default portfolioData;
+
   
